628132256dea3ff387bff19d2942c333e4b38de7
Jere Eskelinen <jere20110@student.hamk.fi>
2021-01-11 19:11:30 +0200

    a

---
+++ asd/haha/ekaprojekti.py
@@ -0,0 +1,24 @@
+'''
+Created on 11.1.2021
+
+@author: jallu
+'''
+
+if __name__ == '__main__':
+    pass
+
+import json
+import urllib.request
+from urllib.request import Request, urlopen
+
+# download raw json object
+req = Request('https://www.sodexo.fi/ruokalistat/output/weekly_json/10229', headers={'User-Agent': 'Mozilla/6.9'})
+webpage = urlopen(req).read()
+
+# parse json object
+obj = json.loads(webpage)
+
+# output all attributes from the site
+print(obj)
+
+
