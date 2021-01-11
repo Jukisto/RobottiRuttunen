cb4bded5d7780db85d5e766ae7ff37e813272520
Jere Eskelinen <jere20110@student.hamk.fi>
2021-01-12 00:25:56 +0200

    Koodi luo nyt ruokalista.txt:n ja kirjoittaa JSON tiedoston järkevästi tekstimuotoon.

--- asd/haha/ekaprojekti.py
+++ asd/haha/ekaprojekti.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     pass
 
 import json
+import pprint
 from urllib.request import Request, urlopen
 
 # download raw json object
@@ -18,6 +19,11 @@ webpage = urlopen(req).read()
 obj = json.loads(webpage)
 
 # output all attributes from the site
-print(obj)
+# print(obj)
 
 
+#Create ruokalista.txt if it does not exists. Cleans JSON to more readable format.
+with open('ruokalista.txt', 'w') as outfile:
+    pprint.pprint(obj, outfile)
+
+# pprint.pprint(obj)
